ca7f8a91a226e3dc7f182cb20935485d8f767942
Sahithi-kasarla <sahithikasarla123@gmail.com>
2023-11-09 16:12:07 +0530

    assignment 2-p-4

---
+++ assignment 2-p-4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2_p_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks;
+            Console.WriteLine("enter the percentage ");
+            marks = int.Parse(Console.ReadLine());
+            if(marks>=90)
+            {
+                Console.WriteLine("Garde A");
+            }
+            else if(marks>=80)
+            {
+                Console.WriteLine("Grade B");
+            }
+            else if(marks>=70)
+            {
+                Console.WriteLine("Grade C");
+            }
+            else if(marks>=60)
+            { 
+                Console.WriteLine("Grade D");
+            }
+            else
+            {
+                Console.WriteLine("Grade F");
+            }
+            Console.ReadKey();
+        }
+    }
+}
